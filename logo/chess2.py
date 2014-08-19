ba1be81005660552ce157c6cf48df268a23aa543
adam1984 <adamhotson1984@gmail.com>
2014-08-19 12:10:59 +0100

    Running logo/chess2.py

--- logo/chess2.py
+++ logo/chess2.py
@@ -2,14 +2,14 @@ from tealight.logo import move, turn, color
 
 def square(edges, size):
   angle = 360/ edges
-  for i in range(0, edges):
+  for i in range(0,200,5 edges):
     move(size)
     turn(angle)
     
 square(10,100)
 
-for i in range(10,200,5):
-  move(i)
-  turn(83)
-  c = colors[(i / 5)%3]
-  color(c)
+#for i in range(10,200,5):
+  #move(i)
+  #turn(83)
+ # c = colors[(i / 5)%3]
+ # color(c)

871a2f4f7b7dfd3f5dcbed90a707e3f02df7955e
adam1984 <adamhotson1984@gmail.com>
2014-08-19 11:52:45 +0100

    Running robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -6,4 +6,13 @@ from tealight.robot import (move,
                             left_side, 
                             right_side)
 
-# Add your code here
+# Add your code heredef spiral(size):
+  
+  if size > 500:
+    return
+  
+  move(size)
+  turn(90)
+  spiral(size + 5)
+  
+spiral(0)

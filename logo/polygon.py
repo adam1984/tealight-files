b7be165fc05941e561e18922782f7fe95193182e
adam1984 <adamhotson1984@gmail.com>
2014-08-19 12:21:49 +0100

    Running logo/polygon.py

--- logo/polygon.py
+++ logo/polygon.py
@@ -2,6 +2,9 @@ from tealight.logo import move, turn
 
 def square(edges, size):
   angle = 360.0 / edges
+
+def sq((edges, size):
+  angle = 360.0 / edges
   
   for i in range(0, edges):
     move(size)
@@ -13,5 +16,5 @@ for y in range(5, edges):
     move(size)
     turn(angle)
 
-square(4,50)
+sq(4,50)
     

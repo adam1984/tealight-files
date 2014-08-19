250cad69d5496b99b992aeebebedab0bc585aba1
adam1984 <adamhotson1984@gmail.com>
2014-08-19 14:50:02 +0100

    Running logo/chess.py

--- logo/chess.py
+++ logo/chess.py
@@ -2,12 +2,17 @@ from tealight.logo import move, turn, color
 
 from random import choice 
 word = choice (["code", "club"])
+
 out =""
+
 for letter in word:
   out = out + "_"
+  
 print("guess a letter in the word, then press enter:", out)
 guess = input()
+
 if guess in word:
   print("well done")
+  
 else:
     print("noooo")

a736bad1a4a29733e40ad51c3798dd215305b053
adam1984 <adamhotson1984@gmail.com>
2014-08-20 09:23:30 +0100

    Running art/factorise.py

--- art/factorise.py
+++ art/factorise.py
@@ -1,1 +1,13 @@
 print "A new file!"
+
+from math import sqrt
+
+def factorise(n)
+  for i in range(2, int(sqrt(n))+1):
+    if n % i ==0
+      print i
+      factorise(n / i)
+      return
+    print n
+    
+    factorise(100)

ffe0fe4ea4724bd403c97a651809539d7b4c69a1
adam1984 <adamhotson1984@gmail.com>
2014-08-20 10:54:46 +0100

    Running art/images.py

--- art/images.py
+++ art/images.py
@@ -1,10 +1,10 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
-x = 0
-y = 150
+x = 150
+y = 0
 
 width = 10
-height = 1
+height = 5
 
 for i in range(0,width):
   for j in range(0,height):

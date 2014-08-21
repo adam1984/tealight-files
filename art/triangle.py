f78a7a6c450063c553d66f2b93b30ddef34a906e
adam1984 <adamhotson1984@gmail.com>
2014-08-21 11:33:38 +0100

    Running art/triangle.py

--- art/triangle.py
+++ art/triangle.py
@@ -12,4 +12,15 @@ def triangle(x,y,size):
   fill_polygon([(x0,y0),(x1,y1),(x2,y2)])
 
 triangle(50,50,50) 
-            
+
+def triangle2(x,y,size):
+  x0 = x + size* sin(0)
+  y0 = y + size* cos(0)
+  x1 = x + size* sin(2*pi/3)
+  y1 = y + size* cos(2*pi/3)
+  x2 = x + size* sin(2* 2*pi/3)
+  y2 = y + size* cos(2* 2*pi/3)
+  
+  fill_polygon([(x0,y0),(x1,y1),(x2,y2)])
+
+triangle2(20,20,20) 

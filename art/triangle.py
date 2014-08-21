0c206007fdaa0946ce7cbd1c04d2e5c7eb724f34
adam1984 <adamhotson1984@gmail.com>
2014-08-21 11:58:14 +0100

    Running art/triangle.py

--- art/triangle.py
+++ art/triangle.py
@@ -12,10 +12,11 @@ def triangle(x,y,size):
   fill_polygon([(x0,y0),(x1,y1),(x2,y2)])
 
 triangle(50,50,50) 
+triangle.setfillcolor('black')
 
-triangle.rotate(angle=pi/4, axis=axis, origin=pos)
-  radians(360) is equal to 2*pi
-  degrees(2*pi) is equal to 360
+#triangle.rotate(angle=pi/4, axis=axis, origin=pos)
+ # radians(360) is equal to 2*pi
+  #degrees(2*pi) is equal to 360 
 
 
 def triangle2(x,y,size):

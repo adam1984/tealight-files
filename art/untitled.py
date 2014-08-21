82e8126cc760368401d09cdf4f6a734ad9b1d6ac
adam1984 <adamhotson1984@gmail.com>
2014-08-21 12:00:27 +0100

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -1,1 +1,12 @@
-print "A new file!"
+def rotatePolygon(polygon,theta):
+    """Rotates the given polygon which consists of corners represented as (x,y),
+    around the ORIGIN, clock-wise, theta degrees"""
+    theta = math.radians(theta)
+    rotatedPolygon = []
+    for corner in polygon :
+        rotatedPolygon.append(( corner[0]*math.cos(theta)-corner[1]*math.sin(theta) , corner[0]*math.sin(theta)+corner[1]*math.cos(theta)) )
+    return rotatedPolygon
+
+
+my_polygon = [(0,0),(1,0),(0,1)]
+print rotatePolygon(my_polygon,90)

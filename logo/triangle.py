40062b3e442388b680b65c3771c6e94371f92575
adam1984 <adamhotson1984@gmail.com>
2014-08-21 11:27:06 +0100

    Running logo/triangle.py

--- logo/triangle.py
+++ logo/triangle.py
@@ -1,1 +1,21 @@
-print "A new file!"
+from tealight.art import fill_polygon
+from math import sin, cos, pi
+
+def triangle(x,y,size):
+  x0 = x + size* sin(0)
+  y0 = y + size* cos(0)
+  x1 = x + size* sin(2*pi/3)
+  y1 = y + size* cos(2*pi/3)
+  x2 = x + size* sin(2* 2*pi/3)
+  y2 = y + size* cos(2* 2*pi/3)
+  
+
+fill_polygon([x0,y0),(x1,y1),(x2,y2)])
+
+triangle(50,50,50)
+              
+              
+
+
+              
+

fd19d356aee1fa5a5016be759abf2a623c0166c6
adam1984 <adamhotson1984@gmail.com>
2014-08-21 12:33:27 +0100

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -1,13 +1,43 @@
-import math
-def rotatePolygon(polygon,theta):
-    """Rotates the given polygon which consists of corners represented as (x,y),
-    around the ORIGIN, clock-wise, theta degrees"""
-    theta = math.radians(theta)
-    rotatedPolygon = []
-    for corner in polygon :
-        rotatedPolygon.append(( corner[0]*math.cos(theta)-corner[1]*math.sin(theta) , corner[0]*math.sin(theta)+corner[1]*math.cos(theta)) )
-    return rotatedPolygon
+from tealight.art import box,line_width,polygon,color,fill_polygon,clear,screen_width,screen_height
+from math import sin, cos, pi
+from tealight.utils import sleep
 
 
-my_polygon = [(0,0),(1,0),(0,1)]
-print rotatePolygon(my_polygon,90)
+def triangle(x,y,size,angle=0):
+  pts = []
+  for i in range(0,3):
+    theta = angle + i*2*pi/3
+    pts.append((x + size*sin(theta),
+                y + size*cos(theta)))
+    
+  polygon(pts)
+
+def fill_triangle(x,y,size,angle=0):
+  pts = []
+  for i in range(0,3):
+    theta = angle + i*2*pi/3
+    pts.append((x + size*sin(theta),
+                y + size*cos(theta)))
+    
+  fill_polygon(pts)
+  
+
+m_a = 0
+
+line_width(3)
+def handle_frame():
+  global m_a
+  
+  color("white")
+  box(0,0,screen_width,screen_height)
+  
+  for i in range(0,25,1):
+    color("hsl(0,100%," + str(i*4) + "%)")
+    fill_triangle(200,200,200-i*6,2*i*m_a)
+  
+    color("hsl(0,100%," + str(i*4-20) + "%)")
+    triangle(200,200,200-i*6,2*i*m_a)
+    
+  m_a += 0.002
+  
+  sleep(30)

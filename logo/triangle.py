3c74ca08df4807a223eb23514b17204472465ff0
adam1984 <adamhotson1984@gmail.com>
2014-08-21 11:27:37 +0100

    Running logo/triangle.py

--- logo/triangle.py
+++ logo/triangle.py
@@ -9,8 +9,7 @@ def triangle(x,y,size):
   x2 = x + size* sin(2* 2*pi/3)
   y2 = y + size* cos(2* 2*pi/3)
   
-
-  fill_polygon([x0,y0),(x1,y1),(x2,y2)])
+  fill_polygon([(x0,y0),(x1,y1),(x2,y2)])
 
 triangle(50,50,50)
               
